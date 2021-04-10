307557fab7c88df614abcaedcba023b1ddf86974
veryacademy <veryacademy@gmail.com>
2021-04-10 16:33:40 +0100

    nc

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -111,10 +111,11 @@ USE_TZ = True
 
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
