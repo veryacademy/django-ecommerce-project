ff8ca63563530b3577910e4c78400a85d56e2da8
veryacademy <veryacademy@gmail.com>
2021-04-10 17:08:52 +0100

    nc

--- ecommerce/wsgi.py
+++ ecommerce/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for ecommerce project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
