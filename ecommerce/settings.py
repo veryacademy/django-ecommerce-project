7477329cf655a6b3610dae7dbe1f22c63f29aae5
veryacademy <veryacademy@gmail.com>
2021-04-10 18:03:46 +0100

    nc

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "staticfiles"),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "staticfiles"),)
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
