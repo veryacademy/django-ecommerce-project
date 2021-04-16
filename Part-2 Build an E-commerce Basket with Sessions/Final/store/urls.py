8acc6833ab63e60fba1bd6d8f08bf060e1aafa67
veryacademy <veryacademy@gmail.com>
2021-04-16 20:36:38 +0100

    nc

---
+++ Part-2 Build an E-commerce Basket with Sessions/Final/store/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'store'
+
+urlpatterns = [
+    path('', views.product_all, name='product_all'),
+    path('<slug:slug>', views.product_detail, name='product_detail'),
+    path('shop/<slug:category_slug>/', views.category_list, name='category_list'),
+]
