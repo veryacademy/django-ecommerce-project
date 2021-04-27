336f2b7a2d5c9e57789c7cd5d52ca798d3a7aead
veryacademy <veryacademy@gmail.com>
2021-04-27 13:15:22 +0100

    nc

--- Part-10 Pytest Testing 1/ecommerce/settings.py
+++ Part-10 Pytest Testing 1/ecommerce/settings.py
@@ -111,8 +111,8 @@ USE_TZ = True
 
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, "staticfiles"),)
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
